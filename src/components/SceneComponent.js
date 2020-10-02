b99ad27cbcc4a6c444eb6801f4dae8c9228bf189
sainiajay <saini.ajay172@gmail.com>
2020-10-02 18:42:24 +0530

    Train is clicking!

--- src/components/SceneComponent.js
+++ src/components/SceneComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import * as THREE from "three"
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 import { GLTFLoader }  from 'three/examples/jsm/loaders/GLTFLoader'
-// import Stats from 'three/examples/jsm/libs/stats.module';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const initScene = (canvas) => {
     //Setup Rederer
@@ -24,8 +24,15 @@ const initScene = (canvas) => {
     const camera = new THREE.PerspectiveCamera(45, width / height, 10, 1000);
     camera.position.set(0, 0, 20);
 
+    //Controls
+    const controls = new OrbitControls( camera, renderer.domElement );
+    controls.target.set( 0, 0.5, 0 );
+    controls.update();
+    controls.enablePan = false;
+    controls.enableDamping = true;
+
     //Setup Light
-    scene.add( new THREE.HemisphereLight( 0xffffff, 0x000000, 0.4 ) );
+    scene.add( new THREE.HemisphereLight( 0xffffff, 0x000000, 0.6 ) );
     const dirLight = new THREE.DirectionalLight( 0xffffff, 1 );
     dirLight.position.set( 5, 2, 8 );
     scene.add( dirLight );
@@ -39,6 +46,15 @@ const initScene = (canvas) => {
     //The Clock
     const clock = new THREE.Clock();
 
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
+    let clicked = false;
+    const onMouseMove = ( event ) => {
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        clicked = true;
+    }
+
     // Load Texture
     const path = 'park/';
     const format = '.jpg';
@@ -50,11 +66,13 @@ const initScene = (canvas) => {
     
     //Callbacks
     const onAssetLoad = ( gltf ) => {
-        console.log('loaded');
+        console.log('loaded', gltf);
         const model = gltf.scene;
         model.position.set( 1, 1, 0 );
         model.scale.set( 0.01, 0.01, 0.01 );
+        const children = [];
         model.traverse( ( child ) => {
+            children.push(child);
             if ( child.isMesh ) {
                 child.material.envMap = envMap;
             }
@@ -67,12 +85,25 @@ const initScene = (canvas) => {
             requestAnimationFrame( animate );
             const delta = clock.getDelta();
             mixer.update( delta );
+            controls.update();
+            if(clicked) {
+                raycaster.setFromCamera( mouse, camera );
+                const intersects = raycaster.intersectObjects( children );
+                for(const intersectedEl of intersects) {
+                    if ( intersectedEl.object.name.includes('Object675_') ) {
+                        console.log('Train clicked!');
+                        window.open('https://autocode.app', '_blank');
+                        break;
+                    }
+                }
+                clicked = false;
+            }
             renderer.render( scene, camera );
         }
-
         animate();
     }
     loader.load('LittlestTokyo.glb', onAssetLoad, console.log, console.error);
+    window.addEventListener( 'click', onMouseMove, false );
     return renderer;
 }
 
